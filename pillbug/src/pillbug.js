e8996e7ef341679476831c1a592947d8aa40d4e4
Andrew Buchan <andyhasit@gmail.com>
2018-08-23 14:14:18 +0100

    started on change back to pure js

--- pillbug/src/pillbug.js
+++ pillbug/src/pillbug.js
@@ -1,553 +1,107 @@
-/*
-
-Aim:
-
-  Allow rapid prototyping, then optimising.
-
-New strategy:
-
-The render function assumes nothing. You can
-
-The top level box might not be bound to an element:
-
-  this.bind('menu', box(Menu, this.menuVisible))
-  this.bind('page-content', box(PageContainer, this.menuVisible))
-  //but how do I prevent it from building new instances?
-
-
-
-Ideas:
-  box accepts: data, differ, key
-  don't bother detecting changes in props
-  Make render accept arguments, such as
-    this 
-    the root app
-    functions h, b etc...
-    the element?
-    whether it is firstRender or not
-
-
-New version: 
-  - there is no flush, only update, which calls render.
-  - render does the DOM updating, so no need to collect child boxes or anything.
-
-  Redraw happens in a tree like fashion
-
-  Use FastDom?
-
-render() {
-  
-}
-
-
-
-*/
-
 const c = console
-const splitter = '---'
 
 
+//For constructing DOM
 class NodeWrapper {
-  constructor(element) {
-    this.element = element
+  constructor(tag) {
+    this.el = document.createElement(tag)
   }
   atts(atts) {
     for (let key in atts) {
       //Todo, check if different, and remove uneeded
-      this.element.setAttribute(key, atts[key])
+      this.el.setAttribute(key, atts[key])
     }
+    return this
+  }
+  clear() {
+    this.el.innerHTML = ''
     return this
   }
   on(listeners) {
     for (let key in listeners) {
-      this.element.addEventListener(key, listeners[key])
+      this.el.addEventListener(key, listeners[key])
     }
     return this
   }
   inner(inner) {
-    //Accepts an array of elements ready to be bound
-    if (Array.isArray(inner)) {
-      //Todo: would be good to detach all children
-      //This improves speed first time round, but if I then go caching and
-      // reusing elements
-      this.element.innerHTML = ''
-      let fragment = document.createDocumentFragment()
-      inner.forEach((child) => {
-        if (child instanceof NodeWrapper) {
-          // Node has already been updated
-          fragment.appendChild(child.element)
-        } else {
-          fragment.appendChild(document.createTextNode(child))
-        }
-      })
-      this.element.appendChild(fragment)
-    } else if (inner instanceof NodeWrapper) {
-      this.element.innerHTML = ''
-      this.element.appendChild(inner.element)
-    } else if (inner !== undefined) {
-      //https://stackoverflow.com/questions/21311299/nodevalue-vs-innerhtml-and-textcontent-how-to-choose
-      let oldHtml = this.element.textContent
-      let newHtml = typeof inner == "string" ? inner : inner.toString()
-      if (oldHtml !== newHtml) {
-        this.element.innerHTML = newHtml
-      }
+    this.el.innerHTML = ''
+    if (!Array.isArray(inner)) {
+      inner = [inner]
     }
-  }
-}
-
-
-class Box extends NodeWrapper {
-  constructor(props, key) {
-    super()
-    this._key = key
-    this._boxCache = {}
-    this._elementCache = {}
-    this.update(props)
-  }
-  update(props) {
-    if (props !== this.props) {
-      this._dirty = true
-    }
-    this.props = props
-    this.render()
-  }
-  b() {
-    
-  }
-  h(tag, atts, inner, events, key) {
-    //returns a NodeWrapper
-    let element = document.createElement(tag)
-    let wrapper = new NodeWrapper(element)
-    wrapper.atts(atts)
-    wrapper.inner(inner)
-    wrapper.on(events)
-    return wrapper
-  }
-  render() {
-    m.tag('div')
-    //Top level box will have element set
-    m.atts()
-    this.inner()
-    `
-    div
-      ul
-        li
-
-    `
-  }
-  tag(type, atts) {
-    if (this.element == undefined) {
-      this.element = document.createElement(type)
-    }
-    if (atts !== undefined) {
-      this.atts(atts)
-    }
-  }
-}
-
-
-function createElement(tag) {
-  return document.createElement(tag)
-}
-
-class VirtualNode {
-  constructor(tag, atts, inner, listeners) {
-    this.tag = tag.toUpperCase()
-    this.inner = inner
-    this.atts = atts
-    this.listeners = listeners
-  }
-}
-
-class OldBox {
-  constructor(data) {
-    this._data = data
-    this._dirty = true
-    this._childBoxes = []
-    this._boxCache = {}
-  }
-  static getKey() {
-    if (this.singleton) {
-      return 'singleton'
-    }
-    if (this.trackBy !== undefined && arguments.length > 0) {
-      return arguments[0][this.trackBy]
-    }
-  }
-  push(data) {
-    if (data !== data) {
-      this._dirty = true
-    }
-      this._dirty = true
-    this._data = data
-  }
-  flush(force) {
-    if (force || this._dirty) {
-      this._redraw()
-    }
-    //todo: cancel this if current element is not visible.
-    this._childBoxes.forEach(function(box) {
-      box.flush()
-    })
-  }
-  _redraw() {
-    let vTree = this.render()
-    if (this.element == undefined) {
-      this.element = createElement(vTree.tag)
-    }
-    this._childBoxes.length = 0
-    this._updateNode(this.element, vTree, this._childBoxes)
-    this._dirty = false
-  }
-  _updateNode(element, vTree, childBoxes) {
-    this._updateElement(element, vTree.atts)
-    if (vTree.listeners) {
-      this._updateListeners(element, vTree.listeners)
-    }
-    let inner = vTree.inner
-    if (Array.isArray(inner)) {
-      this._processArray(element, inner, childBoxes)
-    } else {
-      if (inner instanceof Box) {
-        this._processBox(inner, childBoxes)
-        element.appendChild(inner.element)
-      } else if (inner instanceof VirtualNode) {
-        let child = createElement(inner.tag)
-        this._updateNode(child, inner, childBoxes)
-        element.innerHTML = ''
-        element.appendChild(child)
-      } else if (inner !== undefined) {
-        let oldHtml = element.innerHTML
-        let newHtml = typeof inner == "string" ? inner : inner.toString()
-        if (oldHtml !== newHtml) {
-          element.innerHTML = newHtml
-        }
-      }
-    }
-  }
-  _processBox(box, childBoxes) {
-    if (box.element == undefined) { 
-      // if child box was never bound, render it now.
-      box._redraw()
-    } else {
-      // else add to childBoxes to be flushed once done here.
-      childBoxes.push(box)
-    }
-  }
-  _processArray(element, children, childBoxes) {
-    /*
-    This function gets called when inner is an array.
-    */
     let fragment = document.createDocumentFragment()
-    children.forEach((child) => {
-      let childElement
-      if (child instanceof Box) {
-        this._processBox(child, childBoxes)
-        childElement = child.element
-      } else if (child instanceof VirtualNode) {
-        childElement = createElement(child.tag)
-        this._updateNode(childElement, child, childBoxes)
+    inner.forEach(child => {
+      if (child instanceof NodeWrapper || child instanceof View) {
+        fragment.appendChild(child.el)
+      } else if (child instanceof Node) {
+        fragment.appendChild(child)
       } else {
-        //childElement = document.createTextNode(child);
-        //Maybe https://developer.mozilla.org/en-US/docs/Web/API/range/createContextualFragment
-        childElement = createElement('div')
-        childElement.innerHTML = child
+        fragment.appendChild(document.createTextNode(child.toString()))
       }
-      fragment.appendChild(childElement)
-    });
-    element.innerHTML = ''
-    element.appendChild(fragment)
-  }
-  _updateElement(element, atts) {
-    for (let key in atts) {
-      element.setAttribute(key, atts[key])
-    }
-  }
-  _updateListeners(element, listeners) {
-    for (let key in listeners) {
-      element.addEventListener(key, listeners[key])
-    }
-  }
-  _addCache(cls, keyFn) {
-    //todo: improve
-    this._boxCache[cls.name] = {
-      getKey: function() {
-
-      },
-      boxes: {}
-    }
-  }
-  _(cls, ...args) {
-    let className = cls.name
-    let key = cls.getKey.apply(cls, args)
-    if (key == undefined) {
-      return new cls(...args)
-    }
-    if (!this._boxCache.hasOwnProperty(className)) {
-      this._boxCache[className] = {}
-    }
-    let register = this._boxCache[className]
-    if (register.hasOwnProperty(key)) {
-      let box = register[key]
-      box.push.apply(box, args)
-      return box
-    } else {
-      let box = new cls(...args)
-      box._key = key
-      register[key] = box
-      return box
-    }
-  }
-}
-
-/*
-class ViewModel {
-  constructor(props) {
-    this._watchers = []
-    this._changes = []
-    for (var key in props) {
-      this[key] = props[key]
-    }
-  }
-  flush() { 
-    var _this = this;
-    this._watchers.forEach(function(watcher){
-      watcher.push(_this);
-      watcher.flush();
-    });
-    this._changes.length = [];
-  }
-  action(name, fn) {
-    let _this = this;
-    this[name] = function() {
-      let result = fn.apply(_this, arguments)
-      c.log(77)
-      _this.flush()
-      return result
-    }
-  }
-  bind(cls, id) {
-    var box = new cls(this)
-    box.element = document.getElementById(id)
-    this._watchers.push(box)
-  }
-}
-*/
-
-class TopLevelContainer extends Box {
-  constructor(id) {
-    super()
-    this._id = id
-    this.element = document.getElementById(id)
-  }
-}
-
-class ModalContainer extends TopLevelContainer {
-  render() {
-    if (this._currentModal) {
-      return h.div({id: 'modal-container', style: 'display: block;'}, this._currentModal)
-    } else {
-      return h.div({id: 'modal-container', style: 'display: hidden;'}, '')
-    }
-  }
-  showModal(cls, params) {
-    this._currentModal = this._(cls, params)
-    this._redraw()
-    return new Promise((resolve, reject) => {
-      this._currentModal.promise
-        .then(result => {
-          this._currentModal = undefined
-          this._redraw()
-          resolve(result)
-        })
-        .catch(error => {
-          this._currentModal = undefined
-          this._redraw()
-          reject(error)
-        })
     })
+    this.el.appendChild(fragment)
+    return this
+  }
+  text(text) {
+    this.el.textContent = text
+    return this
   }
 }
 
-class RootBox extends Box {
+const h = function(tag) {
+  return new NodeWrapper(tag)
+}
+
+class View {
+  constructor(model, props, key) {
+    this.draw(model, this, h, props, key)
+  }
+  update(m, v, h, p) {
+
+  }
+  root(el) {
+    if (el instanceof NodeWrapper || el instanceof View) {
+      this.el = el.el
+    } else if (el instanceof Node) {
+      this.el = el
+    } else {
+      throw new TypeError("View.root() only accepts types: NodeWrapper, View, Node")
+    }
+  }
+}
+
+class App {
   constructor() {
-    super()
-    this.html = {}
-    this.Box = Box
-    this.ModalContainer = ModalContainer
-    this.TopLevelContainer = TopLevelContainer
+    this._register = {}
   }
-  flush(force) {
-    this._redraw()
-    this._childBoxes.forEach(function(box) {
-      box.flush()
-    })
+  action() {
   }
-  addTags(elements) {
-    let h = this.html
-    elements.forEach(function(tag) {
-      h[tag] = function(atts, inner, listeners) {
-        return new VirtualNode(tag, atts, inner, listeners)
-      }
-    })
-  }
-  action(name, fn) {
-    let _this = this;
-    this[name] = function() {
-      let result = fn.apply(_this, arguments)
-      _this.flush()
-      return result
+  _getWatchList(event) {
+    let watchers = this._register[event]
+    if (watchers == undefined) {
+      watchers = []
+      this._register[event] =  watchers
     }
+    return watchers
+  }
+  emit(event, data) {
+    this._getWatchList(event).forEach(w => w(data))
+  }
+  on(event, callback) {
+    this._getWatchList(event).push(callback)
   }
 }
 
 
-class IndentationError extends Error {}
-
-class Block {
-  constructor(m, props) {
-    this.element = undefined
-    this._draw()
-    //this.update(m, props)
-  }
-  _draw() {
-    let el, indent, text,  parent, startIndent, previousIndent, parentStack = []
-    this.constructor.template.split('\n').forEach(line => {
-      [indent, text] = this._parseIndent(line)
-      if (indent !== -1) {
-        // Set initial indent
-        if (previousIndent == undefined) {
-          previousIndent = indent
-          startIndent = indent
-        } else {
-          if (startIndent == indent) {
-            throw new IndentationError("You cannot have more than one top level element")
-          }
-        }
-        // Determine if we have moved up or down an indent
-        if (indent > previousIndent) {
-          parentStack.push(el)
-        } else if (indent < previousIndent) {
-          //We might want to check indent matches what is expected
-          parentStack.pop()
-        }
-        // Determine what kind of object we're dealing with
-        if (text.startsWith(':')) {
-          el = this._processBlock(text)
-        } else {
-          el = this._processElement(text)
-        }
-        // Add to parent, or set as block's own element
-        if (parentStack.length == 0) {
-          this.element = el
-          parentStack.push(el)
-        } else {
-          parentStack[parentStack.length - 1].appendChild(el)
-        }
-        previousIndent = indent
-      }
-    })
-  }
-  _processElement(line) {
-    /*let el, tag, rest
-    [tag, ...rest] = this._splitLine(line)
-    el = document.createElement(tag)
-    return el
-
-    */
-    let el, html, extra, template = document.createElement('template');
-    //console.log(this._parseLine(line)[0])
-    [html, extra] = this._parseLine(line);
-    let v = this._parseLine(line);
-    template.innerHTML = html;
-    el = template.content.firstChild;
-    return el;
-  }
-  _parseLine(line) {
-    let html, extra, tag, pos = line.search(/---/);
-    if (pos == -1) {
-      html = line
-    } else {
-      html = line.substring(0, pos).trim();
-      extra = line.substring(pos + 3).trim()
-    }
-    pos = html.indexOf(' ')
-    if (pos == -1) {
-      tag = html
-    } else {
-      tag = html.substring(0, pos).trim()
-    }
-    html = `<${html}></${tag}>`;
-    return [html, extra]
-  }
-  _parseIndent(line) {
-    let pos = line.search(/\S/)
-    return [pos, line.substring(pos).trim()]
-  }
-  update(m, props) {
-
-  }
-
-}
+const pillbug = {}
 
 
-const pillbug = {}//new RootBox()
-
-
-let tags = 'a b button br div form h1 h2 h3 h4 h5 li i img input label p section span table td th tr ul'
-//pillbug.addTags(tags.split(' '));
-
+pillbug.App = App
+pillbug.View = View
+pillbug.NodeWrapper = NodeWrapper
 pillbug.version = '0.0.1'
-pillbug.splitter = splitter
-pillbug.VirtualNode = VirtualNode
-pillbug.Box = Box
-pillbug.Block = Block
-pillbug.IndentationError = IndentationError
 module.exports = pillbug
 
-
-
 /*
-
-
-_updateElement(element, atts) {
-  for (var key in atts) {
-    let val = atts[key]
-    if (key.startsWith('on') && val.startsWith('@')) {
-      let call = val.slice(1)
-      val = `${this._eventPrefix()}.${call}`
-    }
-    element.setAttribute(key, val);
-  }
-}
-_eventPrefix() {
-  if (this._eventPrefixStr == undefined) {
-    let c = this.constructor
-    this._eventPrefixStr = `pillbug._box(${c.name}, '${this._key}')`
-  }
-  return this._eventPrefixStr
-}
-
-
-
-Gets used by Box register to establish the key
-Note that this function has no access to the object itself.
-'this' resolves to the prototype (like a static method)
-*/
-
-
-/*
-Gets called whenever the box is requested during parent _redraw.
-Arguments will be same as init but with changes pushed in front.
-
-This implementation only checks if data has changed.
-*/
-/*
-Recursively applies a definition tree to an element
-Definition can be:
-- Box
-- VirtualNode
-- 
 
 
 function htmlToElement(html) {
--- pillbug/tests/test.js
+++ pillbug/tests/test.js
@@ -1,196 +1,106 @@
 const pillbug = require('../dist/pillbug.js')
 const c = console
 
-/*
 test('Pillbug version test', () => {
   expect(pillbug.version).toBe('0.0.1')
 })
 
-test('Test Box.tag()', () => {
-  
-  class TestBox extends pillbug.Box {
-    render() {
-      this.tag('span', {id: 'my-div'})
+test('Set root to native DOM element works', () => {
+  class MyView extends pillbug.View {
+    draw(m,v,h,p,k) {
+      v.root(document.createElement('span'))
+    }
+  } 
+  let view = new MyView()
+  expect(view.el.tagName).toBe('SPAN')
+})
+
+test('Set root to NodeWrapper works', () => {
+  class MyView extends pillbug.View {
+    draw(m,v,h,p,k) {
+      v.root(h('span'))
+    }
+  } 
+  let view = new MyView()
+  expect(view.el.tagName).toBe('SPAN')
+})
+
+test('Set root to invalid type throws TypeError', () => {
+  class MyView extends pillbug.View {
+    draw(m,v,h,p,k) {
+      v.root(7)
     }
   }
-  let box = new TestBox()
-  expect(box.element.tagName).toBe('SPAN')
-  expect(box.element.id).toBe('my-div')
+  expect(() => {
+    let view = new MyView()
+  }).toThrow(TypeError)
 })
 
+test('Draw with raw DOM operations', () => {
+  class MyView extends pillbug.View {
+    draw() {
+      this.el = document.createElement('span')
+      this.el.textContent = 'hello'
+    }
+  } 
+  let view = new MyView()
+  expect(view.el.tagName).toBe('SPAN')
+  expect(view.el.textContent).toBe('hello')
+})
 
-test('Test Box.inner() with string', () => {
-  class TestBox extends pillbug.Box {
-    render() {
-      this.tag('span', {id: 'my-div'})
-      this.inner('hello')
+test('Draw with h', () => {
+  class MyView extends pillbug.View {
+    draw(m,v,h,p,k) {
+      let root = h('span').text('hello')
+      v.root(root)
     }
   }
-  let box = new TestBox()
-  expect(box.element.textContent).toBe('hello')
+  let view = new MyView()
+  expect(view.el.tagName).toBe('SPAN')
+  expect(view.el.textContent).toBe('hello')
 })
 
-
-test('Test Box.inner() with single element', () => {
-  class TestBox extends pillbug.Box {
-    render() {
-      this.tag('div', {id: 'my-div'})
-      this.inner(this.h('span', {id: 'inner-span'}, 'hello'))
+test('test inner', () => {
+  class MyView extends pillbug.View {
+    draw(m,v,h,p,k) {
+      let root = h('div').inner([
+        h('span').text('hello'),
+        h('span').text('yo')
+        ])
+      v.root(root)
     }
   }
-  let box = new TestBox()
-  expect(box.element.firstChild.id).toBe('inner-span')
-  expect(box.element.firstChild.textContent).toBe('hello')
+  let view = new MyView()
+  expect(view.el.tagName).toBe('DIV')
+  expect(view.el.children.length).toBe(2)
+  expect(view.el.children[0].textContent).toBe('hello')
+  expect(view.el.children[1].textContent).toBe('yo')
+  expect(view.el.children[1].tagName).toBe('SPAN')
 })
 
-test('Test Box.inner() with array of elements', () => {
-  class TestBox extends pillbug.Box {
-    render() {
-      this.tag('div', {id: 'my-div'})
-      this.inner([
-        this.h('span', {id: 'inner-span1'}, 'pill'),
-        this.h('span', {id: 'inner-span2'}, 'bug'),
-      ])
-    }
-  }
-  let box = new TestBox()
-  expect(box.element.children.length).toBe(2)
-  expect(box.element.children[0].id).toBe('inner-span1')
-  expect(box.element.children[1].id).toBe('inner-span2')
-  expect(box.element.children[0].textContent).toBe('pill')
-  expect(box.element.children[1].textContent).toBe('bug')
-})
-
-*/
-test('Block parses template tree correctly', () => {
-  class TestBlock extends pillbug.Block {}
-  TestBlock.template = `
-    div
-      input
-      ul
-        li
-        li
-      span  
-  `
-  let block = new TestBlock()
-  expect(block.element.tagName).toBe('DIV')
-  expect(block.element.children.length).toBe(3)
-  expect(block.element.children[0].tagName).toBe('INPUT')
-  expect(block.element.children[1].tagName).toBe('UL')
-  expect(block.element.children[2].tagName).toBe('SPAN')
-  let ul = block.element.children[1]
-  expect(ul.children.length).toBe(2)
-})
-
-
-test('Block throws error if more than one top-level element', () => {
-  class TestBlock extends pillbug.Block {}
-  TestBlock.template = `
-    div
-      input
-    span  
-  `
-  function foo() {
-    let block = new TestBlock()
-  }
-  expect(foo).toThrowError(pillbug.IndentationError);
-})
-
-test('Block parses attributes', () => {
-  class TestBlock extends pillbug.Block {}
-  TestBlock.template = `
-    span id="my-span" class="btn-alert"
-  `
-  let block = new TestBlock()
-  expect(block.element.tagName).toBe('SPAN')
-  expect(block.element.className).toBe('btn-alert')
-
-})
-
-
-test('Block parses props', () => {
-  class TestBlock extends pillbug.Block {}
-  TestBlock.template = `
-    span id="my-span" --- class:varClass
-  `
-  let block = new TestBlock({varClass:'btn-alert'})
-  expect(block.element.tagName).toBe('SPAN')
-  expect(block.element.className).toBe('btn-alert')
-
+test('App events', () => {
+  var a, b;
+  let app = new pillbug.App()
+  app.on('event1', value => a = value)
+  app.on('event2', value => b = value)
+  app.emit('event1', 1)
+  app.emit('event2', 3)
+  expect(a).toBe(1)
+  expect(b).toBe(3)
 })
 
 
 
-//For constructing DOM
-class NodeWrapper{
-  constructor(tag) {
-    this.element = document.createElement(tag)
-  }
-  atts(atts) {
-    for (let key in atts) {
-      //Todo, check if different, and remove uneeded
-      this.element.setAttribute(key, atts[key])
-    }
-    return this
-  }
-  clear() {
-    this.element.innerHTML = ''
-    return this
-  }
-  on(listeners) {
-    for (let key in listeners) {
-      this.element.addEventListener(key, listeners[key])
-    }
-    return this
-  }
-  inner(inner) {
-    //Accepts an array of elements ready to be bound
-    if (Array.isArray(inner)) {
-      let fragment = document.createDocumentFragment()
-      inner.forEach(child => {
-        if (child instanceof NodeWrapper || child instanceof View) {
-          fragment.appendChild(child.element)
-        } else {
-          fragment.appendChild(document.createTextNode(child))
-        }
-      })
-    }
-    return this
-  }
-  text(text) {
-    this.element.textContent = text
-    return this
-  }
-}
+/*
 
-function h(tag, atts) {
-  let v = new NodeWrapper(tag)
-  if (atts) {
-    v.atts(atts)
-  }
-  return v
-}
-
-
-
-class View {
-
-  constructor() {
-    
-  }
-  draw(m, h, props) {
+draw(m,v,h,p,k) {
     let itemsUl = h('ul')
     let input = h('input').on({'click': e => alert(e)})
-    this.element = h('div').inner([
+    this.el = h('div').inner([
       
       itemsUl
     ])
     m.on('')
   }
-  h() {
-
-  }
-
-
-
-}
+  
+  */
