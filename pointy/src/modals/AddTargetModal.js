85c034cb77f97b4861c917a4335d8235599d36d4
Andrew Buchan <andyhasit@gmail.com>
2018-12-02 01:51:09 +0000

    Added some controls and styles

--- pointy/src/modals/AddTargetModal.js
+++ pointy/src/modals/AddTargetModal.js
@@ -16,7 +16,7 @@ var someFormattedDate = dd + '/'+ mm + '/'+ y;
 
     let today = new Date()
     new Date(today.getFullYear(), 1, 22);
-*/
+
 function getDateSpread() {
   return [
     {text: 'Sat', date: ''},
@@ -24,19 +24,42 @@ function getDateSpread() {
   ]
 }
 
+input type=range provides a visual UI slider to input a number. The UI for this control is browser-dependent.
+
+input type=color
+
+<input list="pasta" autocomplete=off>
+
+<datalist id="pasta">
+<option>Bavette</option>
+<option>Cannelloni</option>
+<option>Fiorentine</option>
+<option>Gnocchi</option>
+<option>Pappardelle</option>
+<option>Penne lisce</option>
+<option>Pici</option>
+<option>Rigatoni</option>
+<option>Spaghetti</option>
+<option>Tagliatelle</option>
+</datalist>
+*/
+
 export default class AddTargetModal extends Modal {
   overlay(h,v,a,p,k,s) {
     return h('div').class('modal-background')
   }
   content(h,v,a,p,k,s) {
     let text = '';
-    let textInput = h('input').class('modal-input modal-autofocus')
+    let textInput = h('input')
+      .class('modal-input modal-autofocus')
+      .atts({list: 'suggestions'})
       .on('change', e => {text = e.target.value})
       .on('keyup', e => {
         if (e.keyCode == 13) {
           s.resolve({text: text})
         }
       })
+    /*
     let dateBtnsDiv = h('div').class('button-row').inner(
       getDateSpread(5).map(datePair => {
         return h('button').text(datePair.text).on('click', e => {
@@ -44,11 +67,16 @@ export default class AddTargetModal extends Modal {
         })
         })
       )
+    */
+    let dataList = h('datalist').id('suggestions').inner(
+      ['a', 'black', 'bling', 'car'].map(o => h('option').inner(o))
+    )
     return h('div').class('modal-content modal-animate').inner([
       h('div').inner([
         textInput,
-        dateBtnsDiv,
-        h('input').atts({'type':'datetime-local'})
+        dataList,
+        h('input').class('modal-input').atts({type:'range', min:0, max:100, value:90, step:10}),
+        h('input').class('modal-input').atts({type:'datetime-local'})
       ]),
       h('button').text('OK').on('click', e => s.resolve({text: text})),
       h('button').text('Cancel').on('click', e => s.reject('user-cancelled')),
